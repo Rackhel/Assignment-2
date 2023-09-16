b694f484d41b6a76224ad15fb39a0142cbf91f45
RFLB <144320840+Rackhel@users.noreply.github.com>
2023-09-16 13:27:52 +0900

    first commit

---
+++ Assignment 2.py
@@ -0,0 +1,32 @@
+print("This is Number 1.\n")
+Hours = input("Enter Hours: ")
+Hours = int(Hours)
+print(Hours,"hours\n")
+
+Rate = input("Enter Rate: ")
+Rate = float(Rate)
+print(Rate,"rate\n")
+
+Salary = Hours * Rate
+print("This is the Salary: ",Salary)
+print("\n")
+
+print("This is Number 2.\n")
+Celsius = input("Enter Celsius: ")
+Celsius = int(Celsius)
+print(Celsius,"degree celsius\n")
+Fahrenheit = (Celsius * 9 / 5 + 32)
+print("This is the Fahrenheit: ",Fahrenheit)
+print("\n")
+
+print("This is Number 3.\n")
+Seconds = input("Enter Seconds: ")
+Seconds = float(Seconds)
+print("\n")
+Hour = (Seconds / 3600)
+print(Hour,"= This is the seconds in terms of hours.\n")
+Minute = (Seconds / 60)
+print(Minute,"= This is the seconds in terms of minutes.\n")
+print("therefore,the inputted Seconds is,",Hour,"hours,",Minute,"minutes,",Seconds,"seconds.")
+print("\n")
+print("Rackhel Fernando Lovies Byanto, 202312229, Assignment 2.")
