4623ff5a7d62cd2f34f77c298e7967b227af3667
RFLB <144320840+Rackhel@users.noreply.github.com>
2023-09-16 20:24:31 +0900

    Change wrong calculations

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,3 +1,4 @@
+#Number 1
 print("This is Number 1.\n")
 Hours = input("Enter Hours: ")
 Hours = int(Hours)
@@ -11,6 +12,7 @@ Salary = Hours * Rate
 print("This is the Salary: ",Salary)
 print("\n")
 
+#Number 2
 print("This is Number 2.\n")
 Celsius = input("Enter Celsius: ")
 Celsius = int(Celsius)
@@ -19,14 +21,14 @@ Fahrenheit = (Celsius * 9 / 5 + 32)
 print("This is the Fahrenheit: ",Fahrenheit)
 print("\n")
 
+#Number 3
 print("This is Number 3.\n")
 Seconds = input("Enter Seconds: ")
 Seconds = float(Seconds)
 print("\n")
-Hour = (Seconds / 3600)
-print(Hour,"= This is the seconds in terms of hours.\n")
-Minute = (Seconds / 60)
-print(Minute,"= This is the seconds in terms of minutes.\n")
-print("therefore,the inputted Seconds is,",Hour,"hours,",Minute,"minutes,",Seconds,"seconds.")
+Hour = (Seconds // 3600)
+Minutes = ((Seconds % 3600) // 60)
+Seconds1 = (Seconds % 60)
+print("therefore,the inputted Seconds is,",Hour,"hours,",Minutes,"minutes,",Seconds1,"seconds.")
 print("\n")
 print("Rackhel Fernando Lovies Byanto, 202312229, Assignment 2.")
